fa726b01b459248dd5939962cb2e3c0d7a2d1f9c
dipen kukadiya <dipenkukadiya@gmail.com>
2023-12-22 18:14:10 +0530

    trying

--- src/main/java/com/example/TodoApp/controller/LoginController.java
+++ src/main/java/com/example/TodoApp/controller/LoginController.java
@@ -8,11 +8,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping
 public class LoginController {
 
-    @Override
-    @GetMapping
-    public String toString() {
-        return "Welcome to TODO App";
+@RestController
+public class HomeController {
+    @GetMapping("/home")
+    public String home(){
+        return "this is home page";
+
     }
-    
-    
+    @GetMapping("/admin")
+    public String admin(){
+        return "this is admin page";
+    }
+}
 }
