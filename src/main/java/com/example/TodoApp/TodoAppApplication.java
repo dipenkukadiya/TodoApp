f624a364df1489e5ba5e2ec4f906aba3fb52f632
dipen kukadiya <dipenkukadiya@gmail.com>
2024-01-08 13:02:45 +0530

    hjkh

--- src/main/java/com/example/TodoApp/TodoAppApplication.java
+++ src/main/java/com/example/TodoApp/TodoAppApplication.java
@@ -1,13 +1,14 @@
 package com.example.TodoApp;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
-
 @SpringBootApplication
 public class TodoAppApplication {
+
 	public static void main(String[] args) {
-		SpringApplication.run(TodoAppApplication.class,args);
+		SpringApplication.run(TodoAppApplication.class, args);
+
 	}
-		
+
 }
